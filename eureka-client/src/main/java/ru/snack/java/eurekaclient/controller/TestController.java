2592ff62cf79b2c9bf26dcbe9295bf97b8823ee1
Dmitriy Pankratov <7snack7@gmail.com>
2025-09-18 01:24:45 +0700

    add api-gateway

--- eureka-client/src/main/java/ru/snack/java/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/ru/snack/java/eurekaclient/controller/TestController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+
     @GetMapping("/test")
     public String test() {
         return "test";
