8f02a36494f494de4556710b74955c79e8323d03
Dmitriy Pankratov <7snack7@gmail.com>
2025-09-18 23:32:47 +0700

    add config server

---
+++ github-config-server/src/test/java/ru/snack/java/githubconfigserver/GithubConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.snack.java.githubconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
